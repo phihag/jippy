d3d081fff1d9ed3ed631e91c3c9487dcb026d791
Philipp Hagemeister <phihag@phihag.de>
2012-11-21 18:03:54 +0100

    Basic setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='jippy',
+      version='0.0.1',
+      description='Java in pure Python',
+      author='Philipp Hagemeister',
+      author_email='phihag@phihag.de',
+      url='https://github.com/phihag/jippy/',
+      packages=['jippy'],
+      classifiers=[
+          'License :: OSI Approved :: Apache Software License',
+          'Development Status :: 2 - Pre-Alpha',
+          'Programming Language :: Python',
+          'Programming Language :: Java',
+      ]
+)
